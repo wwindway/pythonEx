fa567b0f371a6a0d95ef28cee7ed61beba94d655
developer1 <aaa@aaa.com>
2026-01-14 13:39:20 +0900

    혼공부 기초 추가 식별자 내용 및 문자열 내용

--- ex1.py
+++ ex1.py
@@ -5,3 +5,24 @@ print('혼공부 참고')
 
 import keyword
 print(keyword.kwlist)
+
+# 식별자 처리 변수 함수 처리
+# 변수 식별자 처리는 다음내용
+
+alpha = 10
+alpha10 = 10
+_alpha = 123
+ALPHA = 10
+
+#변수 선언시 캐멀스타일, 스네이트 스타일
+# 캐멀 클래스
+# 스네이트 함수 , 변수용도 많이 사용됨
+
+10 + 20
+print("자바는 연산후 리턴 안받으면 에러지만 연산후 처리되나")
+
+# 출력문처리 print
+print('hello python')
+print(52)
+print( 52, 30 , 'hello')
+print('이름' , ' 처리' ,' 여러글자 출력')
